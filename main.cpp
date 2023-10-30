7641eab999f06101ebc9692ff023d89c94161b10
tejas <tejasanand.iitdcse@gmail.com>
2023-10-30 14:43:51 +0530

    soft

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int main(int argc , char * argv[])
 	cout << "hello" << endl;
 	bn.read_network(argv[1]);
 	bn.read_records(argv[2]);
-	bn.read_correct("gold_alarm.bif");
+	bn.read_correct(argv[3]);
 
-	bn.EM(20);	
+	bn.EM(200);	
 	cout << argv[1] << endl;
 	bn.create_bif(argv[1],"solved_alarm.bif");
 
